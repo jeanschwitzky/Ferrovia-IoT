31c25815a3feb20026bbad5d40f74117d83f6685
LAURA CAROLINA REECK <laura_reeck@estudante.sesisenai.org.br>
2025-09-30 12:53:08 -0300

    WiFi_trem

--- trem/trem.ino
+++ trem/trem.ino
@@ -1,6 +1,15 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <WiFi.h>
+const String SSID = "FIESC_IOT_EDU"; 
+const String PASS = "8120gv08"; 
 
+void setup() {
+  Serial.begin(115200); //configura a placa para mostrar na tela 
+  WiFi.begin(SSID, PASS); //tenta conectar na rede 
+  while (WiFi.status() != WL_CONNECTED){
+    Serial.print("."); 
+    delay (200); 
+  }
+  Serial.println("Conectado com sucesso!");
 }
 
 void loop() {
