4a261a7540daa17f5d41cdb7e14af8d99b9de1a5
JEAN CARLOS SCHWITZKY <jean_schwitzky@estudante.sesisenai.org.br>
2025-09-30 12:51:24 -0300

    Conexação WiFi Motor

--- motor/motor.ino
+++ motor/motor.ino
@@ -1,5 +1,17 @@
+#include <WiFi.h>
+const String SSID = "FIESC_IOT_EDU";
+const String PASS = "8120gv08";
+
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200); //configura a placa para mostrar na tela
+  WiFi.begin(SSID, PASS); // tenta conectar na rede
+  Serial.println("Conectando no Wifi");
+  while(WiFi.status() != WL_CONNECTED){
+    Serial.print(".");
+    delay(200);
+  }
+  Serial.println("Conectado com sucesso");
 
 }
 
