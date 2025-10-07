84baa1b1e3eded33777941b3e07e66338fadb9bf
JEAN CARLOS SCHWITZKY <jean_schwitzky@estudante.sesisenai.org.br>
2025-10-07 12:27:07 -0300

    motor

--- motor/motor.ino
+++ motor/motor.ino
@@ -1,7 +1,17 @@
 #include <WiFi.h>
-const String SSID = "FIESC_IOT_EDU";
+#include <PubSubClient.h>
+
+WiFiClient client; //cria objeto p/ wifi
+PubSubClient mqtt(client); //cria objeto p/ mqtt usando WiFi
+
+const String SSID = "FIESC_IOT_EDU"; 
 const String PASS = "8120gv08";
 
+const String brokerURL = "test.mosquitto.org";
+const int brokerPort = 1883;
+
+const String brokerUser = ""; //variável para o user do broker
+const String brokerPass = ""; //variável para a senha do broker
 
 void setup() {
   Serial.begin(115200); //configura a placa para mostrar na tela
@@ -13,6 +23,17 @@ void setup() {
   }
   Serial.println("Conectado com sucesso");
 
+  mqtt.setServer(brokerURL.c_str(),brokerPort);
+  Serial.println("Conectando no Broker");
+
+  String boardID = "Motor-"; //cria um nome que começa com "Motor-"
+  boardID += String(random(0xffff),HEX); // junta o "Motor-" com um número aleatório Hexadecimal
+  
+  while(!mqtt.connect(boardID.c_str())){ //Enqunato ão estiver conectado mostra "."
+    Serial.print(".");
+    delay(200);
+  }
+  Serial.println("Conectado com sucesso ao broker!");
 }
 
 void loop() {
