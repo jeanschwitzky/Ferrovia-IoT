b3e2d45481d521a960f608b0e036778cc94e1bf7
CARLOS AUGUSTO NARLOCH <carlos_narloch@estudante.sesisenai.org.br>
2025-10-07 12:54:40 -0300

    Principal

--- Principal/Principal.ino
+++ Principal/Principal.ino
@@ -1,8 +1,8 @@
 #include <WiFi.h>
 #include <PubSubClient.h>
 
-WiFiClient client; //cria objeto p/ wifi
-PubSubClient mqtt(client); //cria objeto p/ mqtt usando WiFi
+WiFiClient client;          //cria objeto p/ wifi
+PubSubClient mqtt(client);  //cria objeto p/ mqtt usando WiFi
 
 const String SSID = "FIESC_IOT_EDU";
 const String PASS = "8120gv08";
@@ -10,25 +10,25 @@ const String PASS = "8120gv08";
 const String brokerURL = "test.mosquitto.org";
 const int brokerPort = 1883;
 
-const String brokerUser = ""; //variavel para o user do broker
-const String brokerPass = ""; //variavel para senha do broker  
+const String brokerUser = "";  //variavel para o user do broker
+const String brokerPass = "";  //variavel para senha do broker
 void setup() {
-  Serial.begin(115200); //configura a placa para mostrar na tela
-  WiFi.begin(SSID,PASS); //tenta conectar na rede 
+  Serial.begin(115200);    //configura a placa para mostrar na tela
+  WiFi.begin(SSID, PASS);  //tenta conectar na rede
   Serial.println("Conectando no WiFi");
-  while(WiFi.status() != WL_CONNECTED){
+  while (WiFi.status() != WL_CONNECTED) {
     Serial.print(".");
     delay(200);
   }
   Serial.println("Conectado com sucesso");
-  mqtt.setServer(brokerURL.c_str(),brokerPort);
+  mqtt.setServer(brokerURL.c_str(), brokerPort);
   Serial.println("conectando no Broker");
-//Cria um nome que comeca com principal
+  //Cria um nome que comeca com principal
   String boardID = "Principal-";
-//junta o "Principal" com um numero aleatorio hexadecimal
-boardID += String(random(0xffff),HEX);
+  //junta o "Principal" com um numero aleatorio hexadecimal
+  boardID += String(random(0xffff), HEX);
 
-  while(mqtt.connect(boardID.c_str())){
+  while (!mqtt.connect(boardID.c_str())) {
     Serial.print(".");
     delay(200);
   }
@@ -36,6 +36,9 @@ boardID += String(random(0xffff),HEX);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  String msg = "Carlos: Bezinho";  // Informação que será enviadada para o broker
+  String topico = "AulaIoT/msg";
+  mqtt.publish(topico.c_str(), msg.c_str());
+  delay(2000);
+  mqtt.loop();
 }
