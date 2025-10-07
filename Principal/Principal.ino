8e6b4bfb9b01464315a569213d058764f4968242
CARLOS AUGUSTO NARLOCH <carlos_narloch@estudante.sesisenai.org.br>
2025-10-07 12:29:25 -0300

    Principal

--- Principal/Principal.ino
+++ Principal/Principal.ino
@@ -1,7 +1,17 @@
 #include <WiFi.h>
+#include <PubSubClient.h>
+
+WiFiClient client; //cria objeto p/ wifi
+PubSubClient mqtt(client); //cria objeto p/ mqtt usando WiFi
+
 const String SSID = "FIESC_IOT_EDU";
 const String PASS = "8120gv08";
 
+const String brokerURL = "test.mosquitto.org";
+const int brokerPort = 1883;
+
+const String brokerUser = ""; //variavel para o user do broker
+const String brokerPass = ""; //variavel para senha do broker  
 void setup() {
   Serial.begin(115200); //configura a placa para mostrar na tela
   WiFi.begin(SSID,PASS); //tenta conectar na rede 
@@ -11,7 +21,18 @@ void setup() {
     delay(200);
   }
   Serial.println("Conectado com sucesso");
+  mqtt.setServer(brokerURL.c_str(),brokerPort);
+  Serial.println("conectando no Broker");
+//Cria um nome que comeca com principal
+  String boardID = "Principal-";
+//junta o "Principal" com um numero aleatorio hexadecimal
+boardID += String(random(0xffff),HEX);
 
+  while(mqtt.connect(boardID.c_str())){
+    Serial.print(".");
+    delay(200);
+  }
+  Serial.println("Conectado  com sucesso ao broker");
 }
 
 void loop() {
