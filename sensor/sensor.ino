e6593bb3abde8c29f7623bb97a7b3a92632b192f
ARTHUR FERREIRA WIEST <arthur_wiest@estudante.sesisenai.org.br>
2025-10-28 11:30:29 -0300

    sensor

--- sensor/sensor.ino
+++ sensor/sensor.ino
@@ -1,13 +1,15 @@
 #include <WiFi.h>
 #include <PubSubClient.h>
+#include <WiFiClientSecure.h>
 #include "env.h"
 
-WiFiClient client;            //cria objeto p/ wifi
-PubSubClient mqtt(client);    //cria objeto p/ mqtt usando WiFi
+WiFiClientSecure wifiClient;            //cria objeto p/ wifi
+PubSubClient mqttClient(wifiClient);    //cria objeto p/ mqtt usando WiFi
 
 void setup() {
   Serial.begin(115200); //configura a placa para mostrar na tela
   WiFi.begin(SSID, PASS); //tenta conectar na rede
+  wifiClient.setInsecure();
   Serial.println("Conectando no Wifi");
   while(WiFi.status() != WL_CONNECTED){
     Serial.print(".");
